c82d2726ea1736744b515b13fcfa3501885b66d4
Emma Mearls <em5083a@student.american.edu>
2018-09-02 20:30:47 -0400

    User Input Exercise
    In class lab exercise...

---
+++ Test 3.py
@@ -0,0 +1,12 @@
+str = input("enter favorite place to eat: ")
+
+food = input("enter favorite food: ")
+
+beverage = input("enter favorite beverage: ")
+
+print("I like to eat at", str, "I usually get the", food, "and", beverage)
+
+# assign variable first
+# add input + what you want the user to see
+# enter answers below in python console
+# answers will then be plugged in when printed 
